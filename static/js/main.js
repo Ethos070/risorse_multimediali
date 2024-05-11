356e7bc941319b5b65858bfab34f0ebbf58e362c
Ethos070 <64336910+Ethos070@users.noreply.github.com>
2024-05-11 15:56:09 +0200

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -4,10 +4,18 @@ let day = date.getDate();
 let month = date.getMonth() + 1;
 let year = date.getFullYear();
 
-// This arrangement can be altered based on how we want the date's format to appear.
+if(Number(day)<10){
+	day = "0" + day
+}
+if(Number(month)<10){
+	month = "0" + month
+}
 let currentDate = `${year}-${month}-${day}`;
 
+let b = 0;
+
 function prenota() {
+    if(b==0){
     document.getElementById("box").innerHTML += `
     <h2>Prenota</h2>
     <form action="#" method="post">
@@ -15,4 +23,6 @@ function prenota() {
         <input type="date" name="data_inizio" min="${currentDate}" required>
         <input type="submit" value="submit">
     </form>`;
+    b = b + 1;
+    }
 }
