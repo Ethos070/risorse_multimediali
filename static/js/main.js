774cd2f4d930932e50695390b67dcc814101a229
Ethos070 <64336910+Ethos070@users.noreply.github.com>
2024-05-11 15:38:11 +0200

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,18 @@
-const prenotazione = document.getElementById("prenotazione");
-const box = document.getElementById("box");
+const date = new Date();
+
+let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+
+// This arrangement can be altered based on how we want the date's format to appear.
+let currentDate = `${year}-${month}-${day}`;
 
 function prenota() {
     document.getElementById("box").innerHTML += `
     <h2>Prenota</h2>
     <form action="#" method="post">
         <label for="data">Data prenotazione:</label>
-        <input type="date" name="data_inizio" required>
+        <input type="date" name="data_inizio" min="${currentDate}" required>
         <input type="submit" value="submit">
     </form>`;
 }
